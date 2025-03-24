f331b551ad423003136f7b6e8868f3e2dbc51fce
taarinikadambi <27tkadambi@castilleja.org>
2025-03-24 14:01:24 -0700

    .kbj,hvgcjfhx

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import processing.core.PApplet;
+public class Main extends PApplet{
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args){
+        PApplet.main("Main");
+    }
+
+    public void settings(){
+        size(600,600);
+    }
+
+    public void setup(){
+
+    }
+
+    public void draw(){
+
+    }
+
+    private int binarysearchRecursive(){
+        return 12;
+        //low middle high
+        //change based on number at mid
+        //you know the drill
+        //but how to code?
+        //how to code indeed
     }
 }
